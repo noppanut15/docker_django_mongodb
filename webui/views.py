ac47973581cf6d691d3fd0360526865105183083
Noppanut Ploywong <noppanut15@hotmail.com>
2021-05-18 19:02:27 +0700

    Add a sample index page

---
+++ webui/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+def index(request):
+    context = {
+
+    }
+    return render(request, 'index.html', context)
