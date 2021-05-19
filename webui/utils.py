a2d6211d110e892dc1dfac2e0ea3c8700d147559
Noppanut Ploywong <noppanut15@hotmail.com>
2021-05-19 10:25:06 +0700

    Use pymongo instead

---
+++ webui/utils.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+def get_db_handle(db_name="django_mongodb_docker", host="mongodb", port=27017, username="root", password="mongoadmin"):
+    client = MongoClient(host=host,
+                         port=int(port),
+                         username=username,
+                         password=password
+                        )
+    db_handle = client[db_name]
+    return db_handle, client
+
+
+def get_collection_handle(db_handle, collection_name):
+    return db_handle[collection_name]
--- webui/views.py
+++ webui/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from webui.models import *
+from webui.utils import get_db_handle, get_collection_handle
 
 def index(request):
-    all_contacts = Contacts.objects.all()
-    # print(all_contacts)
-    for i in all_contacts:
-        print(i.firstName)
+    db_handle, mongo_client = get_db_handle()
+    collection_handle = get_collection_handle(db_handle, "contacts")
+    data = collection_handle.find({'firstName': 'James'})
+    for x in data:
+        print(x['firstName'])
+
     context = {
 
     }
+
     return render(request, 'index.html', context)
